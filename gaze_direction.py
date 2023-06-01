d03546762d53b7255e87c6ffd41317532036dc0b
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-06-01 20:52:53 +0100

    Small bigfix

--- gaze_direction.py
+++ gaze_direction.py
@@ -3,7 +3,7 @@ import numpy as np
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-img = cv2.imread('test2.jpg')
+#img = cv2.imread('test2.jpg')
 
 detector_params = cv2.SimpleBlobDetector_Params()
 detector_params.filterByArea = True
