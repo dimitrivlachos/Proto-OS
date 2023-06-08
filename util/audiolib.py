75fa91ef216e58ca7dc0eba45e25f5fc14dd7914
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-06-08 22:06:57 +0100

    Implemented all functions necessary

--- util/audiolib.py
+++ util/audiolib.py
@@ -1,7 +1,6 @@
 '''Audio library for voice modulation'''
 
 import numpy as np
-import sounddevice as sd
 
 class AudioLib:
     '''Library of audio processing functions.'''
@@ -35,18 +34,79 @@ class AudioLib:
 
         return output
     
-    def pitch_shift(self, audio, n_steps):
-        '''Apply pitch shifting to the audio using a phase vocoder.'''
+    def time_stretch(self, audio, factor, is_spectrum=False, out_spectrum=False):
+        '''Apply time stretching to the audio using a phase vocoder.'''
         # Compute the short-time Fourier transform of the audio
-        spectrum = self.stft(audio)
+        # If is_spectrum is True, assume that audio is already a spectrum
+        if is_spectrum:
+            spectrum = audio
+        else:
+            spectrum = self.stft(audio)
 
-        # Calculate the phase shift
-        phase_shift = 2 * np.pi * n_steps * np.arange(self.blocksize) / self.blocksize
+        # Calculate the phase advance
+        phase_advance = np.linspace(0, np.pi * self.blocksize, self.blocksize, endpoint=False)
 
-        # Apply the phase shift to the spectrum
-        shifted_spectrum = spectrum * np.exp(1j * phase_shift)
+        # Calculate the phase delay
+        phase_delay = phase_advance * factor
+
+        # Apply the phase delay to the spectrum
+        delayed_spectrum = spectrum * np.exp(1j * phase_delay)
 
         # Compute the inverse short-time Fourier transform of the spectrum
-        output = self.istft(shifted_spectrum)
+        # If out_spectrum is True, return the spectrum instead of the audio
+        if out_spectrum:
+            output = delayed_spectrum
+        else:
+            output = self.istft(delayed_spectrum)
+
+        return output
+    
+    def resample(self, audio, factor, is_spectrum=False, out_spectrum=False):
+        '''Resample the audio by a given factor.'''
+        # Compute the short-time Fourier transform of the audio
+        # If is_spectrum is True, assume that audio is already a spectrum
+        if is_spectrum:
+            spectrum = audio
+        else:
+            spectrum = self.stft(audio)
+
+        # Calculate the new length of the spectrum
+        new_length = int(len(spectrum) * factor)
+
+        # Resample the spectrum
+        resampled_spectrum = np.zeros((new_length, self.blocksize), dtype=np.complex_)
+        for i in range(new_length):
+            resampled_spectrum[i] = spectrum[int(i / factor)]
+
+        # Compute the inverse short-time Fourier transform of the spectrum
+        # If out_spectrum is True, return the spectrum instead of the audio
+        if out_spectrum:
+            output = resampled_spectrum
+        else:
+            output = self.istft(resampled_spectrum)
+
+        return output
+    
+    def pitch_shift(self, audio, semitones, is_spectrum=False, out_spectrum=False):
+        # Compute the short-time Fourier transform of the audio
+        # If is_spectrum is True, assume that audio is already a spectrum
+        if is_spectrum:
+            spectrum = audio
+        else:
+            spectrum = self.stft(audio)
+
+        # Stretch the spectrum by a factor corresponding to the pitch shift
+        factor = 2 ** (semitones / 12)
+        stretched_spectrum = self.time_stretch(spectrum, factor, is_spectrum=True, out_spectrum=True)
+
+        # Resample the spectrum to the original length
+        resampled_spectrum = self.resample(stretched_spectrum, 1 / factor, is_spectrum=True, out_spectrum=True)
+
+        # Compute the inverse short-time Fourier transform of the spectrum
+        # If out_spectrum is True, return the spectrum instead of the audio
+        if out_spectrum:
+            output = resampled_spectrum
+        else:
+            output = self.istft(resampled_spectrum)
 
         return output
