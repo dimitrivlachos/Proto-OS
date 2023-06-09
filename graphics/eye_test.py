5a6a841bdd41e3941d73412a13a77da9e4e4edb1
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-06-09 21:25:22 +0100

    Improvements

--- graphics/eye_test.py
+++ graphics/eye_test.py
@@ -13,7 +13,7 @@ joystick.init()
 
 # Initialize the unicorn hat
 unicornhathd.rotation(0)
-unicornhathd.brightness(0.5)
+unicornhathd.brightness(0.6)
 
 # Initialize the dot color
 r = 255
@@ -46,16 +46,16 @@ while True:
     print("y_axis: " + str(y_axis))
 
     # Map the joystick axes to a dot position with 0 being the center
-    x = int(constrain((x_axis + 1) * 8, 1, 15))
-    y = int(constrain((y_axis + 1) * 8, 1, 15))
+    x_short = int(constrain((x_axis + 1) * 8, 1, 15))
+    y_short = int(constrain((y_axis + 1) * 8, 1, 15))
 
     x_actual = []
     y_actual = []
 
-    x_actual.append(x)
-    x_actual.append(x - 1)
-    y_actual.append(y)
-    y_actual.append(y - 1)
+    x_actual.append(x_short)
+    x_actual.append(x_short - 1)
+    y_actual.append(y_short)
+    y_actual.append(y_short - 1)
 
     print("x: " + str(x_actual))
     print("y: " + str(y_actual))
@@ -64,9 +64,12 @@ while True:
     unicornhathd.clear()
 
     # Set the dots color
-    for x in x_actual:
-        for y in y_actual:
-            unicornhathd.set_pixel(x, y, r, g, b)
+    for x in range(0, 16):
+        for y in range(0, 16):
+            if x in x_actual and y in y_actual:
+                unicornhathd.set_pixel(x, y, r, g, b)
+            else:
+                unicornhathd.set_pixel(x, y, 0, 0, 0)
 
     # Show the dot
     unicornhathd.show()
