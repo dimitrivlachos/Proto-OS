485d65e0c3c606a55101049afaf6df4dacfd1bec
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-07-06 23:02:38 +0100

    Updated graphics libraries to draw all shapes

--- libraries/graphics/unicorn_graphics.py
+++ libraries/graphics/unicorn_graphics.py
@@ -5,43 +5,16 @@ class Unicorn_Graphics():
     def __init__(self, rotation=0, brightness=0.5):
         unicornhathd.rotation(rotation)
         unicornhathd.brightness(brightness)
-        
-    def draw_line(self, pixels, x1, y1, x2, y2, r, g, b):
-        '''Draws a line on the 16x16 pixel array'''
-        # Bresenham's line algorithm
-        dx = abs(x2 - x1)
-        dy = abs(y2 - y1)
-        sx = -1 if x1 > x2 else 1
-        sy = -1 if y1 > y2 else 1
-        err = dx - dy
+        unicornhathd.clear()
 
-        while True:
-            pixels[x1][y1] = (r, g, b)
-            if x1 == x2 and y1 == y2:
-                break
-            e2 = 2 * err
-            if e2 > -dy:
-                err -= dy
-                x1 += sx
-            if e2 < dx:
-                err += dx
-                y1 += sy
+    def draw(self, pixels):
+        '''Draws the pixels to the Unicorn HAT HD'''
+        for x in range(16):
+            for y in range(16):
+                r, g, b = pixels[x][y]
+                unicornhathd.set_pixel(x, y, r, g, b)
 
-        return pixels
-    
-    def draw_square(self, pixels, c1, c2, c3, c4, r, g, b):
-        '''Draws a square on the 16x16 pixel array'''
-        pixels = self.draw_line(pixels, c1[0], c1[1], c2[0], c2[1], r, g, b)
-        pixels = self.draw_line(pixels, c2[0], c2[1], c3[0], c3[1], r, g, b)
-        pixels = self.draw_line(pixels, c3[0], c3[1], c4[0], c4[1], r, g, b)
-        pixels = self.draw_line(pixels, c4[0], c4[1], c1[0], c1[1], r, g, b)
-
-        # Fill in the square
-        for x in range(c1[0], c3[0]):
-            for y in range(c1[1], c3[1]):
-                pixels[x][y] = (r, g, b)
-
-        return pixels
+        unicornhathd.show()
     
 class Circle():
     '''Circle object for drawing on the Unicorn HAT HD'''
@@ -134,20 +107,33 @@ class Line():
         return pixels
 
 class Square():
-    def __init__(self, center_x, center_y, side_length, rgb=(255,255,255), pixels=None, filled=True, display_x=16, display_y=16):
+    def __init__(self, c1, c2, c3, c4, side_length, rgb=(255,255,255), pixels=None, filled=True, display_x=16, display_y=16):
         # Center coordinates of the square
-        self.center_x = center_x
-        self.center_y = center_y
-        self.side_length = side_length
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
+        self.c4 = c4
 
-    def draw(self):
-        '''Draws the square on the 16x16 pixel array'''
-        if self.filled:
-            # If filled is True, draw the square and return the pixels
-            return self.draw_square(self.pixels, self.center_x, self.center_y, self.side_length, self.r, self.g, self.b)
-        else:
-            # If filled is False, draw the square and then draw a smaller square on top of it
-            self.pixels = self.draw_square(self.pixels, self.center_x, self.center_y, self.side_length, self.r, self.g, self.b)
-            return self.draw_square(self.pixels, self.center_x, self.center_y, self.side_length - 1, 0, 0, 0)
+        # RGB values of the square
+        self.r = rgb[0]
+        self.g = rgb[1]
+        self.b = rgb[2]
+
+        # Draw the square into the 16x16 pixel array
+        self.draw_square(self.pixels, self.c1, self.c2, self.c3, self.c4, self.r, self.g, self.b, self.filled)
         
-        
+    def draw_square(self, pixels, c1, c2, c3, c4, r, g, b, filled):
+        '''Draws a square on the 16x16 pixel array'''
+        # Draw the square
+        pixels = Line(c1[0], c1[1], c2[0], c2[1], r, g, b, pixels=pixels).pixels
+        pixels = Line(c2[0], c2[1], c3[0], c3[1], r, g, b, pixels=pixels).pixels
+        pixels = Line(c3[0], c3[1], c4[0], c4[1], r, g, b, pixels=pixels).pixels
+        pixels = Line(c4[0], c4[1], c1[0], c1[1], r, g, b, pixels=pixels).pixels
+
+        # Fill in the square
+        if filled:
+            for x in range(c1[0], c3[0]):
+                for y in range(c1[1], c3[1]):
+                    pixels[x][y] = (r, g, b)
+
+        return pixels
