4e126793794f688d10ab752677422f74719b78ed
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-07-08 03:25:24 +0100

    Debugging

--- libraries/graphics/unicorn_graphics.py
+++ libraries/graphics/unicorn_graphics.py
@@ -151,16 +151,19 @@ class Quad():
         # Combine the four matrices
         pixels = s1 + s2 + s3 + s4
 
+        print(pixels)
+
         # Fill in the Quad
         if filled:
             for x in range(16):
                 fill = False
                 for y in range(16):
-                    r, g, b = pixels[x][y]
-                    if r == self.r and g == self.g and b == self.b:
+                    r_, g_, b_ = pixels[x][y]
+                    if r_ == r and g_ == g and b_ == b:
                         fill = not fill
                     if fill:
                         pixels[x][y] = (r, g, b)
             
+        print(pixels)
 
         return pixels
