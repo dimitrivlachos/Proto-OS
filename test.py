fd0a43a0d68e221e02cd0aa116f948d9f9baed73
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-11-21 23:33:50 +0000

    Updated test

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import libraries.graphics.unicorn_graphics as unicorn_graphics
+import numpy as np
 
 u = unicorn_graphics.Unicorn_Graphics()
 
 # Draw a circle
-circle = unicorn_graphics.Circle(8, 8, 8, rgb=(255, 0, 0))
-
-# Draw the circle on the Unicorn HAT HD
-u.draw(circle.draw())
+circle1 = unicorn_graphics.Circle(8, 8, 8, rgb=(255, 0, 0))
 
 # Draw a smaller circle on top of the first circle
-circle = unicorn_graphics.Circle(8, 8, 7, rgb=(0, 0, 0), filled=False)
+circle2 = unicorn_graphics.Circle(8, 8, 6, rgb=(0, 0, 255), filled=False)
 
 #Draw a square
-square = unicorn_graphics.Square(0, 0, 16, rgb=(0, 255, 0))
+square = unicorn_graphics.Quad((1,1), (1,5), (5,5), (5,1), rgb=(0, 255, 0))
+
+#print(type(square.pixels))
+
+pixels = unicorn_graphics.overlay(circle2.pixels, circle1.pixels)
+pixels = unicorn_graphics.overlay(square.pixels, pixels)
+
+u.draw(pixels)
