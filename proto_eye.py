db7fd6b99d9816455d5750ec5829f1f0ddd20847
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-11-23 22:31:54 +0000

    Updated eye test

--- proto_eye.py
+++ proto_eye.py
@@ -5,11 +5,11 @@ import libraries.graphics.unicorn_graphics as ug
 u = ug.Unicorn_Graphics()
 
 # Draw a circle
-sclera = ug.Circle(8, 8, 4, rgb=(255, 192, 203))
+sclera = ug.Circle(8, 8, 6, rgb=(255, 192, 203))
 
 # Draw a quad for the eyebrow
-eyebrow = ug.Quad((0, 0), (0, 15), (8, 0), (9, 15), rgb=(0, 0, 0), pixels=sclera.pixels)
+eyebrow = ug.Quad((0, 0), (0, 15), (6, 15), (12, 0), filled=True, rgb=(255, 0, 0))
 
+pixels = ug.overlay(eyebrow.pixels, sclera.pixels)
 
-
-u.draw(eyebrow.pixels)
+u.draw(pixels)
