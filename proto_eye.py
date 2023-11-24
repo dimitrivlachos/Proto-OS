1ed8bc31b6ea393fc06daae5831f6229d3bc1637
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-11-24 22:04:39 +0000

    Updated new eye code

--- proto_eye.py
+++ proto_eye.py
@@ -2,14 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import libraries.graphics.unicorn_graphics as ug
 
-u = ug.Unicorn_Graphics()
+d = ug.Display()
 
 # Draw a circle
 sclera = ug.Circle(8, 8, 6, rgb=(255, 192, 203))
 
 # Draw a quad for the eyebrow
-eyebrow = ug.Quad((0, 0), (0, 15), (6, 15), (12, 0), filled=True, rgb=(255, 0, 0))
+eyebrow = ug.Polygon([(0, 0), (0, 15), (6, 15), (12, 0)], filled=True, rgb=(255, 0, 0))
 
-pixels = ug.overlay(eyebrow.pixels, sclera.pixels)
-
-u.draw(pixels)
+d.set(sclera.pixels)
+import time
+time.sleep(1)
+d.draw_on_top(eyebrow.pixels)
